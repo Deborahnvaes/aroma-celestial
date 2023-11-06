5a543ecfcf079b8c5f743d9bdd20ba4a295cb341
Kel Malafaia <kelvenymalafaia@gmail.com>
2023-11-06 16:59:19 -0300

    section-info

---
+++ src/app/componentes/section-info/video.tsx
@@ -0,0 +1,14 @@
+
+import { Thumbnail } from './thumbnail';
+
+export function Video({ video }) {
+  return (
+    <div>
+      <Thumbnail video={video} />
+      <a href={video.url}>
+        <h3>{video.title}</h3>
+        <p>{video.description}</p>
+      </a>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import { Header } from "./componentes/header";
+import { SectionInfo } from "./componentes/section-info/section-info";
+import { Video } from "./componentes/section-info/video";
 
 
 export default function Home() {
   return (
     <>
     <Header />
-
-    </>
+    <SectionInfo/>
+   
+</>
+    
 
     
   )
