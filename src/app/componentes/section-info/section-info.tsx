56e7f64d148709d40994bbee7c02d0de62524804
Déborah Novaes <110925099+Deborahnvaes@users.noreply.github.com>
2023-11-07 08:12:25 -0300

    refactor: refatoração e adição de tag.

--- src/app/componentes/section-info/section-info.tsx
+++ src/app/componentes/section-info/section-info.tsx
@@ -1,24 +1,17 @@
-import '@/app/componentes/nav/nav.css'
+import "../../componentes/section-info/stylesInfo.css";
 import tiposCafe from '@/app/componentes/section-info/img/aroma-cafe.png';
 import { Image } from 'react-bootstrap';
-import { Video } from '@/app/componentes/section-info/video';
-
-
+import React from 'react';
 
 export function SectionInfo (){
     return(
-        <>
         <div className='info-site'>
-            <h2>Você conhece os tipos de café?</h2>
-            <p>Talvez você já deve ter ouvido falar expressões como especial, extraforte, tradicional ou gourmet para se referir à qualidade dos cafés. No entanto, se tudo isso ainda é uma novidade para você, queremos te mostrar as principais diferenças dessas bebidas e como são classificadas de forma prática.</p>
-
-            <Image src={tiposCafe.src} alt='piramide'></Image>
-            <Video ></Video>
-
-
-        </div>
-        </>
-        
-        
+            <h2>Você conhece os <strong>tipos de café?</strong></h2>
+            <p>Talvez você já deve ter ouvido falar expressões como especial, <br /> extraforte, tradicional ou gourmet para se referir à qualidade dos <br /> cafés. No entanto, se tudo isso ainda é uma novidade para você, <br /> queremos te mostrar as principais diferenças dessas bebidas e como <br />são classificadas de forma prática.</p>
+           <div className="image-page">
+                <Image src={tiposCafe.src} alt='piramide'></Image>
+                <iframe width="500" height="315" src="https://www.youtube.com/embed/rP5dFR9PKGE?si=_ddGBRkh7Lzwdw2o" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+            </div>       
+    </div>
   );
 }
