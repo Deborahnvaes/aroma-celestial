1789a8c3f7f75cbc3e66e780401cc3b254578e45
Deborah Lyra <deborahp.lyra@gmail.com>
2023-11-06 21:02:36 -0300

    finalizando h1

--- src/app/componentes/header/header.tsx
+++ src/app/componentes/header/header.tsx
@@ -7,13 +7,14 @@ import './style.css'
 function Header () {
     return(
         <header className='header'>
-        <nav>
+        <nav className='nav-header'>
             <ul className='nav-list'>
                 <li className='nav-list-item'>Home</li>
                 <li className='nav-list-item'>Contatos</li>
                 <li className='nav-list-item'>Sobre</li>
             </ul>
         </nav>
+        <h1>AROMA CELESTIAL</h1>
         </header>
     )
 }
