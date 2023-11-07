ee2723d05ac68994d526619d0a97c3d6082c320f
Déborah Novaes <110925099+Deborahnvaes@users.noreply.github.com>
2023-11-07 10:13:18 -0300

    refactor: modificando o nav para JSX

--- src/app/componentes/header/header.tsx
+++ src/app/componentes/header/header.tsx
@@ -1,21 +1,24 @@
 import Image from 'next/image';
 import React from 'react';
-
 import './style.css'
 
+const botoesNav = ['Home', 'Contatos', 'Sobre'];
 
-function Header () {
+export function Header () {
     return(
-        <header className='header'>
-        <nav>
-            <ul className='nav-list'>
-                <li className='nav-list-item'>Home</li>
-                <li className='nav-list-item'>Contatos</li>
-                <li className='nav-list-item'>Sobre</li>
-            </ul>
-        </nav>
-        </header>
-    )
+          <header className="header">
+            <nav>
+              <ul>
+                <div className="botoes-nav">
+                  {botoesNav.map((item, index) => (
+                    <button key={index} className="button">
+                      <li>{item}</li>
+                    </button>
+                  ))}
+                </div>
+              </ul>
+            </nav>
+            <div className="nome-site">AROMA CELESTIAL</div>
+          </header>
+        );
 }
-
-export default Header
