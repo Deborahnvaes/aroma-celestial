e1c475be396c6ef8d49dc0f0c3d4521b8d2c3bec
Déborah Novaes <110925099+Deborahnvaes@users.noreply.github.com>
2023-11-08 11:58:20 -0300

    Correção ortográfica

--- src/app/componentes/section-info/section-info.tsx
+++ src/app/componentes/section-info/section-info.tsx
@@ -6,8 +6,8 @@ import React from 'react';
 export function SectionInfo (){
     return(
         <div className='info-site'>
-            <h2 className="titulo-section">Você conhece os <strong>tipos de café?</strong></h2>
-            <p className="paragrafo-section">Talvez você já deve ter ouvido falar expressões como especial, <br /> extraforte, tradicional ou gourmet para se referir à qualidade dos <br /> cafés. No entanto, se tudo isso ainda é uma novidade para você, <br /> queremos te mostrar as principais diferenças dessas bebidas e como <br />são classificadas de forma prática.</p>
+            <h2 className="titulo-section">Você conhece os <strong>tipos de cafés?</strong></h2>
+            <p className="paragrafo-section">Talvez você já deva ter ouvido falar expressões como especial, <br /> extraforte, tradicional ou gourmet para se referir à qualidade dos <br /> cafés. No entanto, se tudo isso ainda é uma novidade para você, <br /> queremos te mostrar as principais diferenças dessas bebidas e como <br />são classificadas de forma prática.</p>
            <div className="image-page">
                 <iframe width="500" height="315" src="https://www.youtube.com/embed/AfJNnliddu0?si=TUoG_VXZCYb-wCWD" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
                 <Image src={tiposCafe.src} alt='piramide'></Image>
